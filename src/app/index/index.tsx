b9dddb6849c7745559cf1cd8107cf0310a60b21c
Seu Nome <seuemail@example.com>
2025-02-22 15:33:42 -0300

    ✨ feat: creating modal

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,23 +1,25 @@
-import { Image, View, TouchableOpacity, FlatList } from "react-native";
+import {
+  Image,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  Text,
+} from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
-import { style } from "./style";
+import { styles } from "./style";
 import { colors } from "@/styles/colors";
 import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
 
 export default function Index() {
   return (
-    <View style={style.container}>
-      <View style={style.header}>
-        <Image source={require("@/assets/logo.png")} style={style.logo} />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
         <TouchableOpacity>
-          <MaterialIcons
-            name="add"
-            size={32}
-            color={colors.green[300]}
-            style={style.addButton}
-          />
+          <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
       <Categories />
@@ -32,10 +34,28 @@ export default function Index() {
             onDetails={() => console.log("clicou")}
           />
         )}
-        style={style.links}
-        contentContainerStyle={style.linksContent}
+        style={styles.links}
+        contentContainerStyle={styles.linksContent}
         showsVerticalScrollIndicator={false}
       />
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+              <TouchableOpacity>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.modallinkName}>Douglas</Text>
+            <Text style={styles.modalUrl}>https://douglasresende.com.br</Text>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
--- src/app/index/style.ts
+++ src/app/index/style.ts
@@ -1,7 +1,7 @@
 import { colors } from "@/styles/colors";
 import { StyleSheet } from "react-native";
 
-export const style = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 62,
@@ -26,5 +26,37 @@ export const style = StyleSheet.create({
     gap: 20,
     padding: 24,
     paddingBottom: 100,
+  },
+  modal: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  modalContent: {
+    backgroundColor: colors.gray[900],
+    borderTopWidth: 1,
+    borderTopColor: colors.gray[800],
+    paddingBottom: 42,
+    padding: 24,
+  },
+  modalHeader: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 32,
+  },
+  modalCategory: {
+    flex: 1,
+    fontSize: 16,
+    fontWeight: "500",
+    color: colors.gray[400],
+  },
+  modallinkName: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: colors.gray[200],
+  },
+  modalUrl: {
+    fontSize: 14,
+    color: colors.gray[400],
   },
 });
